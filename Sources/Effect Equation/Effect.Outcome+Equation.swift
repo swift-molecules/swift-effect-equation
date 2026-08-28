0767166025a760138072c6b5dabd3ad4dbd3517e
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-29 00:05:44 +0200

    Use intrinsic Outcome equality

--- Sources/Effect Equation/Effect.Outcome+Equation.swift
+++ Sources/Effect Equation/Effect.Outcome+Equation.swift
@@ -5,30 +5,4 @@ extension Effect::Effect.Outcome: @retroactive Equation::Equation.`Protocol`
 where
     Value: Equation::Equation.`Protocol` & ~Copyable,
     Failure: Equation::Equation.`Protocol`
-{
-
-    public static func == (lhs: borrowing Self, rhs: borrowing Self) -> Bool {
-        switch lhs {
-        case .resumed(let lv):
-            switch rhs {
-            case .resumed(let rv): return lv == rv
-            case .threw: return false
-            case .aborted: return false
-            }
-
-        case .threw(let le):
-            switch rhs {
-            case .resumed: return false
-            case .threw(let re): return le == re
-            case .aborted: return false
-            }
-
-        case .aborted:
-            switch rhs {
-            case .resumed: return false
-            case .threw: return false
-            case .aborted: return true
-            }
-        }
-    }
-}
+{}
